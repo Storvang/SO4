94292dd77edc8e72793984e73b34c6f2e8cfe674
kasper <kasp051h@edu.aarhustech.dk>
2020-11-23 11:14:00 +0100

    Rasmus here you go

--- main.py
+++ main.py
@@ -47,32 +47,68 @@ class window0:
 
 
     def drawWindow(self):
-        window = tk.Tk()
-        window.title("SO4")
+        def differentialregning():
+            window = tk.Tk()
+            window.title("SO4")
 
-        frame_1 = tk.Frame(window, height=300, width=300)
-        frame_2 = tk.Frame(frame_1, height=300, width=300)
+            frame_1 = tk.Frame(window, height=300, width=300, bg="yellow")
 
-        frame_1.grid()
-        frame_2.grid(column=1)
+            a = tk.Label(frame_1, text="Graf/frame 2")
 
-        a = tk.Label(frame_2, text="Frame 2")
-        a.pack()
+            funktion_label = tk.Label(window, text="Intast funktion:")
+            x_value_label = tk.Label(window, text="Intast x værdi:")
+            tekst = tk.Label(window, text="noget tekst")
+            funktion = tk.Entry(window)
+            x_value = tk.Entry(window)
+            btn1 = tk.Button(
+                window,
+                text="fuck me"
+                )
 
-        tekst = tk.Label(frame_1, text="noget tekst")
-        funktion = tk.Entry(frame_1)
-        x_value = tk.Entry(frame_1)
-        btn1 = tk.Button(
-            frame_1
-            text="fuck me"
+            tekst.grid(column=1, row=0)
+            funktion.grid(column=1, row=1)
+            x_value.grid(column=1, row=2)
+            btn1.grid(column=1, row=3)
+            funktion_label.grid(column=0, row=1)
+            x_value_label.grid(column=0, row=2)
+
+            a.grid()
+            frame_1.grid(column=2, row=0, rowspan=4)
+
+            window.mainloop()
+
+        def integralregning():
+            window1 = tk.Tk()
+            window1.title("SO4")
+
+            frame_1 = tk.Frame(window1, height=300, width=300, bg="yellow")
+
+            a = tk.Label(frame_1, text="Graf/frame 2")
+
+            funktion_label = tk.Label(window1, text="Intast funktion:")
+            x_value_label = tk.Label(window1, text="Intast x værdi:")
+            tekst = tk.Label(window1, text="noget tekst")
+            funktion = tk.Entry(window1)
+            x_value = tk.Entry(window1)
+            btn1 = tk.Button(
+                window1,
+                text="fuck me"
             )
 
-        tekst.grid(column=0)
-        funktion.grid(column=0)
-        x_value.grid(column=0)
-        btn1.grid(column=0)
+            tekst.grid(column=1, row=0)
+            funktion.grid(column=1, row=1)
+            x_value.grid(column=1, row=2)
+            btn1.grid(column=1, row=3)
+            funktion_label.grid(column=0, row=1)
+            x_value_label.grid(column=0, row=2)
 
-        window.mainloop()
+            a.grid()
+            frame_1.grid(column=2, row=0, rowspan=4)
+
+            window1.mainloop()
+
+        def thorbjørns_hygge():
+
 
     def askingForVar(self):
         pass
