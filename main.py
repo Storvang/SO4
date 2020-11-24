fd1bac372a4b87b7ab894acf0ae75888b391659b
60390982 <60390982+BoBByN1337@users.noreply.github.com>
2020-11-24 08:56:19 +0100

    With calc and gui

--- main.py
+++ main.py
@@ -88,17 +88,129 @@ class window0:
         self.entry = entry
         self.label = label
 
-    def drawWindow(self):
-        pass
 
+
+    def drawWindow():
+        def differentialregning():
+            window = tk.Tk()
+            window.title("SO4")
+
+            frame_1 = tk.Frame(window, height=300, width=300)
+
+            a = tk.Label(frame_1, text="Graf/frame 2")
+
+            funktion_label = tk.Label(window, text="Intast funktion:")
+            x_value_label = tk.Label(window, text="Intast x værdi:")
+            tekst = tk.Label(window, text="differentialregning")
+            funktion = tk.Entry(window)
+            x_value = tk.Entry(window)
+            btn1 = tk.Button(
+                window,
+                text="fuck me"
+                )
+
+            tekst.grid(column=1, row=0)
+            funktion.grid(column=1, row=1)
+            x_value.grid(column=1, row=2)
+            btn1.grid(column=1, row=3)
+            funktion_label.grid(column=0, row=1)
+            x_value_label.grid(column=0, row=2)
+
+            a.grid()
+            frame_1.grid(column=2, row=0, rowspan=4)
+
+            window.mainloop()
+
+        def integralregning():
+            window1 = tk.Tk()
+            window1.title("SO4")
+
+            frame_1 = tk.Frame(window1, height=300, width=300)
+
+            a = tk.Label(frame_1, text="Graf/frame 2")
+
+            funktion_label = tk.Label(window1, text="Intast funktion:")
+            a_value_label = tk.Label(window1, text="Intast a:")
+            b_value_label = tk.Label(window1, text="Intast b:")
+            tekst = tk.Label(window1, text="integralregning")
+            funktion = tk.Entry(window1)
+            a_value = tk.Entry(window1)
+            b_value = tk.Entry(window1)
+            btn1 = tk.Button(
+                window1,
+                text="fuck me"
+            )
+
+            tekst.grid(column=1, row=0)
+            funktion.grid(column=1, row=1)
+            a_value.grid(column=1, row=2)
+            b_value.grid(column=1, row=3)
+            btn1.grid(column=1, row=4)
+            funktion_label.grid(column=0, row=1)
+            a_value_label.grid(column=0, row=2)
+            b_value_label.grid(column=0, row=3)
+
+            a.grid()
+            frame_1.grid(column=2, row=0, rowspan=4)
+
+            window1.mainloop()
+
+        def thorbjørns_hygge():
+            window2 = tk.Tk()
+            window2.title("SO4")
+
+            frame_1 = tk.Frame(window2, height=300, width=300)
+
+            a = tk.Label(frame_1, text="Graf/frame 2")
+
+            funktion_label = tk.Label(window2, text="Intast funktion:")
+            a_value_label = tk.Label(window2, text="Intast:")
+
+            tekst = tk.Label(window2, text="Thorbjørns hygge program")
+            funktion = tk.Entry(window2)
+            a_value = tk.Entry(window2)
+
+            btn1 = tk.Button(
+                window2,
+                text="fuck me"
+            )
+
+            tekst.grid(column=1, row=0)
+            funktion.grid(column=1, row=1)
+            a_value.grid(column=1, row=2)
+
+            btn1.grid(column=1, row=4)
+            funktion_label.grid(column=0, row=1)
+            a_value_label.grid(column=0, row=2)
+
+
+            a.grid()
+            frame_1.grid(column=2, row=0, rowspan=4)
+
+            window2.mainloop()
+
+        def drawmenu():
+            window = tk.Tk()
+            window.title("bob")
+
+            frame = tk.Frame(window)
+            btnbob = tk.Button(frame, text="Differentialregning", command=differentialregning)
+            btnpeter = tk.Button(frame, text="Integralregning", command=integralregning)
+            btnib = tk.Button(frame, text="Thorbjørns_hygge", command=thorbjørns_hygge)
+
+
+            btnbob.grid()
+            btnpeter.grid()
+            btnib.grid()
+            frame.grid()
+            window.mainloop()
+        drawmenu()
+    drawWindow()
     def askingForVar(self):
         pass
 
-
-
 #Kommer ind i gui:
 antal = int(input("Hvor mange kompunenter er det i funktion: "))
-
 
 if antal == 1:
     a = int(input("Hvad er a værdien: "))
